26d836359bc74735ad709e20d07471a49a8119a8
Julian Lum <lumjulian@gmail.com>
2023-02-22 16:08:08 -0800

    add form component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { Fragments }from "react";
 import '/Users/juelz/lighthouse/scheduler/src/components/Appointment/styles.scss'
 import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 
 
 const Appointment = (props) => {
--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import Show from "components/Appointment/Show"
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form"
 import "index.scss";
 
 import Button from "components/Button";
@@ -195,6 +196,20 @@ storiesOf("Button", module)
   message = "Could not delete appointment."
   onClose = {action('onClose')}
   />)
+
+  .add("Form - Create", () => <Form
+  interviewers={interviewers}
+  onSave={action('onSave')}
+  onCancel={action('onCancel')}
+  />)
   
+  .add("Form - Edit", () => <Form
+  name = "Julian"
+  interviewers = {interviewers}
+  interviewer = {3}
+  onSave = {action('onSave')}
+  onCancel = {action('onCancel')}
+  />)
+
 
 

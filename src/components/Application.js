fe14fe073f227ba3fe458484f627481b471c3f31
Julian Lum <lumjulian@gmail.com>
2023-02-23 14:57:33 -0800

    add server api and using axios get to extract data from database

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,26 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios"
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment"
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 const appointments = {
   "1": {
@@ -75,7 +75,15 @@ export default function Application(props) {
   )
     )
 
-  const [day,setDay] = useState("Monday")
+  const [day,setDays] = useState([])
+
+  useEffect(()=>{
+    axios.get( 'api/days')
+    .then((response) => {
+      console.log(response.data)
+      setDays(response.data)
+    })
+  },[])
  
   return(
     <main className="layout">
@@ -88,9 +96,9 @@ export default function Application(props) {
 <hr className="sidebar__separator sidebar--centered" />
 <nav className="sidebar__menu">
 <DayList
-  days={days}
+  days={day}
   value={day}
-  onChange={setDay}
+  onChange={setDays}
 />
 </nav>
 <img

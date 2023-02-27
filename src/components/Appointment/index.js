fac7334f9fe77ef0ae905285ed88104e7e52ce12
Julian Lum <lumjulian@gmail.com>
2023-02-27 15:39:44 -0800

    add visualmode to appointment.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,17 +3,39 @@ import '/Users/juelz/lighthouse/scheduler/src/components/Appointment/styles.scss
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
-import 
+import Form from "./Form";  
+import useVisualMode from "hooks/useVisualMode";
 
 
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE"
 
 const Appointment = (props) => {
- 
+
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show name={props.interview.name} interviewer={props.interview.interviewer.name}
-      /> : <Empty />} 
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)}/>}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+        interviewers= {props.interviewers || []}
+        onCancel = {back}
+        />
+        
+      )}
     </article>
 
   );
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -21,17 +21,3 @@ export const getInterview = (state, interview) => {
   return null; 
 
 }
-
-// interviewers: {
-//   "1": {  
-//     "id": 1,
-//     "name": "Sylvia Palmer",
-//     "avatar": "https://i.imgur.com/LpaY82x.png"
-//   },
-//   "2": {
-//     id: 2,
-//     name: "Tori Malcolm",
-//     avatar: "https://i.imgur.com/Nmx0Qxo.png"
-//   }
-// }
-// };
